9c6bf5f269948a369f8e2105b211b2632b284c7a
Divya1234532 <dd1320983@gmail.com>
2026-04-18 10:08:37 +0530

    Update LambdaExample.java

--- LambdaExample.java
+++ LambdaExample.java
@@ -2,18 +2,30 @@
 interface Math
 {
     int add(int a,int b);
+    //int sub(int a,int b);
+}
+@FunctionalInterface
+interface Exercise
+{
+    void sayHello();
 }
 public class LambdaExample {
 
     public static void main(String[] args) {
-        Math obj=new Math()
+        Math obj=(a,b)->{
+            System.out.println("Adding two numbers");
+            return a+b;
+        };
+        Exercise ex=()->System.out.println("Hello World");
+        ex.sayHello();
+        /*Math obj=new Math()
         {
             @Override
             public int add(int a,int b)
             {
                 return a+b;
             }
-        };
+        };*/
         System.out.println(obj.add(11,22));
     }
 }
